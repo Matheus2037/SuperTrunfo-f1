101c086885fbc10617eccc22a63ba2e13c05dfbc
unknown <matheusdasilvagastaldi@gmail.com>
2025-05-06 10:46:40 -0300

    Trazendo os dados dos pilotos em tela

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { drivers } from './data/drivers.js';
+import { Image } from 'react-native';
 
 export default function App() {
+  const driverID = drivers[0];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{driverID.name}</Text>
+      <Text>{driverID.description}</Text>
+      <Text>{driverID.team}</Text>
+      <Text>{driverID.country}</Text>
+      <Text>{driverID.stars}</Text>
+      <Image source={{ uri: driverID.image }} style={styles.imageSize}></Image>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +25,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageSize: {
+    width: 100,
+    height: 150,
   },
 });
